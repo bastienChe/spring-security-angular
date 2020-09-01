f7f95de7a39491e605dfe8a5c397dc5e08f39c70
Bastien <bchevriaut@gmail.com>
2020-09-01 12:00:22 +0200

    handling login request on server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,13 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @RestController
@@ -29,5 +34,19 @@ public class DemoApplication {
 
 	  public static void main(String[] args) {
 	    SpringApplication.run(DemoApplication.class, args);
+	  }
+
+	  @Configuration
+	  @Order(SecurityProperties.BASIC_AUTH_ORDER)
+	  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	      http
+	        .httpBasic()
+	      .and()
+	        .authorizeRequests()
+	          .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+	          .anyRequest().authenticated();
+	    }
 	  }
 }
